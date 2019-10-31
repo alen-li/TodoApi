e7d50aab26985c47eb4895691ff91d598e170f51
Hua Lee <seed0111@gmail.com>
2019-10-31 14:35:17 +0800

    first change

--- TodoApi/TodoApi/Controllers/WeatherForecastController.cs
+++ TodoApi/TodoApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TodoApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool2", "Mild", "Warm", "Balmy", "Hot1", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
